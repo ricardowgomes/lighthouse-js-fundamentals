044f9d2aa2a4611cc375361b5195cff86ff3887d
Ricardo W. Gomes <ricardowgomes@outlook.com>
2021-05-19 00:22:58 +0000

    new functions added

---
+++ merge.js
@@ -0,0 +1,10 @@
+const merge = function (array1, array2) {
+  const newArray = array1.concat(array2);
+  newArray.sort();
+
+  return newArray;
+}
+
+console.log(merge([4, 5, 6], [1, 2, 3, 4]), "=?", [1, 2, 3, 4, 4, 5, 6]);
+console.log(merge([4], [2, 5, 8]), "=?", [2, 4, 5, 8]);
+console.log(merge([1, 2, 6], []), "=?", [1, 2, 6]);
